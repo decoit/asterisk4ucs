bf9d6d1633e26a3432bf8315bc48a248d227da62
root <root@asterisk4ucs.decoit.dev>
2011-05-17 16:04:26 +0200

    Wartschlangen hinzugefuegt

--- foobar.sh
+++ foobar.sh
@@ -6,6 +6,8 @@ cp /usr/lib/python2.4/site-packages/univention/admin/handlers/phonebook/phoneboo
 
 cp /usr/lib/python2.4/site-packages/univention/admin/handlers/phonebook/phonegroup.py .
 
+cp /usr/lib/python2.4/site-packages/univention/admin/handlers/phonebook/waitingloop.py .
+
 cp /etc/univention/templates/files/etc/ldap/slapd.conf.d/99test .
 
 cp /etc/univention/templates/info/univention-ldap-server.info .
---
+++ waitingloop.py
@@ -0,0 +1,94 @@
+import univention.admin.filter
+import univention.admin.handlers
+import univention.admin.syntax
+
+module = "phonebook/waitingloop"
+childs = 0
+short_description = u"Warteschlange"
+long_description = u"Warteschlange"
+operations = ['add', 'edit', 'remove', 'search', 'move']
+options = {}
+
+layout = [
+	univention.admin.tab('Allgemein', 'Allgemeine Einstellungen', [
+		[ univention.admin.field("commonName") ],
+		[ univention.admin.field("members") ],
+	])
+]
+
+property_descriptions = {
+	"commonName": univention.admin.property(
+		short_description="Name",
+		syntax=univention.admin.syntax.string,
+		identifies=True,
+		required=True
+	),
+	"members": univention.admin.property(
+		short_description="Teilnehmer",
+		syntax=univention.admin.syntax.userDn,
+		multivalue=True
+	),
+}
+
+mapping = univention.admin.mapping.mapping()
+mapping.register("commonName", "cn",
+	None, univention.admin.mapping.ListToString)
+mapping.register("members", "waitingLoopMember")
+
+class object(univention.admin.handlers.simpleLdap):
+	module=module
+
+	def __init__(self, co, lo, position, dn='', superordinate=None,
+			arg=None):
+		global mapping
+		global property_descriptions
+		self.co = co
+		self.lo = lo
+		self.dn = dn
+		self.position = position
+		self._exists = 0
+		self.mapping = mapping
+		self.descriptions = property_descriptions
+		univention.admin.handlers.simpleLdap.__init__(self, co, lo, 
+			position, dn, superordinate)
+
+	def exists(self):
+		return self._exists
+
+	def open(self):
+		univention.admin.handlers.simpleLdap.open(self)
+		self.save()
+
+	def _ldap_pre_create(self):
+		self.dn = '%s=%s,%s' % (
+			mapping.mapName('commonName'),
+			mapping.mapValue('commonName', self.info['commonName']),
+			self.position.getDn()
+		)
+
+	def _ldap_addlist(self):
+		return [('objectClass', ['top', 'waitingLoop' ])]
+
+
+def lookup(co, lo, filter_s, base='', superordinate=None, scope='sub', 
+		unique=False, required=False, timeout=-1, sizelimit=0):
+	filter = univention.admin.filter.conjunction('&', [
+		univention.admin.filter.expression(
+			'objectClass', "waitingLoop")
+	])
+ 
+	if filter_s:
+		filter_p = univention.admin.filter.parse(filter_s)
+		univention.admin.filter.walk(filter_p, 
+			univention.admin.mapping.mapRewrite, arg=mapping)
+		filter.expressions.append(filter_p)
+ 
+	res = []
+	for dn in lo.searchDn(unicode(filter), base, scope, unique, required, 
+			timeout, sizelimit):
+		res.append(object(co, lo, None, dn))
+	return res
+
+def identify(dn, attr, canonical=0):
+	return 'waitingLoop' in attr.get('objectClass', [])
+
