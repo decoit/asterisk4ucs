f4053922764d4c7988d0a5a2c308f8da74267ee6
root <root@asterisk4ucs.decoit.dev>
2011-05-17 16:00:27 +0200

    bugfix II

--- phonegroup.py
+++ phonegroup.py
@@ -34,7 +34,6 @@ mapping = univention.admin.mapping.mapping()
 mapping.register("commonName", "cn",
 	None, univention.admin.mapping.ListToString)
 mapping.register("members", "phoneGroupMember")
-#	None, univention.admin.mapping.ListToString)
 
 class object(univention.admin.handlers.simpleLdap):
 	module=module
