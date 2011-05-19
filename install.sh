39eb46fca75e5382e01968e44f9e4251054089e1
root <root@asterisk4ucs.decoit.dev>
2011-05-19 10:51:58 +0200

    typo gefixt

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cd "`dirname $0`"
 
 UNI_TEMPLATE_PATH=/etc/univention/templates
 UNI_SCHEMA_PATH=/usr/share/univention-ldap/schema
-UNI_MODULE_PATH=/usr/lib/python2.4/site-packages/univention/admin/handers
+UNI_MODULE_PATH=/usr/lib/python2.4/site-packages/univention/admin/handlers
 UNI_ICON_PATH=/usr/share/univention-webui-style/icon
 
 echo "Installing schemata..."
