bd101986d6a12d5acf328717677c0eb7bfd8e9ab
Tristan Bruns <bruns@decoit.de>
2011-07-14 14:40:06 +0200

    Install now also creates default container

--- install.sh
+++ install.sh
@@ -10,37 +10,39 @@ UNI_SCHEMA_PATH=/usr/share/univention-ldap/schema
 UNI_MODULE_PATH=/usr/lib/python2.4/site-packages/univention/admin/handers
 UNI_ICON_PATH=/usr/share/univention-webui-style/icon
 
-echo -en "Installing schema...\t\t"
+echo "Installing schema..."
 cd schema/
 install -m664 asterisk.schema "$UNI_SCHEMA_PATH/"
 install -m664 80asterisk "$UNI_TEMPLATE_PATH/files/etc/ldap/slapd.conf.d/"
 install -m664 asterisk.info "$UNI_TEMPLATE_PATH/info/"
 cd ..
-echo "done."
+echo -e "\t\t\t\t\t\t\tdone."
 
-echo -en "Installing icons...\t\t"
+echo "Installing icons..."
 mkdir -p "$UNI_ICON_PATH/asterisk"
 install -m664 icons/asterisk/* "$UNI_ICON_PATH/asterisk/"
-echo "done."
+echo -e "\t\t\t\t\t\t\tdone."
 
-echo -en "Installing UMC module...\t\t"
+echo "Installing UMC module..."
 mkdir -p "$UNI_MODULE_PATH/asterisk"
 install -m664 frontend/asterisk/* "$UNI_MODULE_PATH/asterisk/"
-echo "done."
+echo -e "\t\t\t\t\t\t\tdone."
 
-echo -en "Creating extended attributes for UMC user module... \t\t"
+echo "Creating extended attributes for UMC user module..."
 sh frontend/user-phone-extension/install.sh
-echo "done."
+echo -e "\t\t\t\t\t\t\tdone."
 
 echo "Restarting slapd..."
 invoke-rc.d slapd restart
-echo "done."
+echo -e "\t\t\t\t\t\t\tdone."
 
-# ldap dir object default dings
+echo "Creating default container for asterisk data..."
+python2.4 frontend/ldapDefaultNode/install.py
+echo -e "\t\t\t\t\t\t\tdone."
 
 echo "Restarting apache2..."
 invoke-rc.d apache2 restart
-echo "done."
+echo -e "\t\t\t\t\t\t\tdone."
 
 echo "Installation successful."
 
