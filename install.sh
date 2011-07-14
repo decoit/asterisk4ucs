0b947cd577cdc06e5e50cf4117eebaa92eda1d02
Tristan Bruns <bruns@decoit.de>
2011-07-14 11:02:10 +0200

    Install-script angepasst

--- install.sh
+++ install.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
-LDAP_SCHEMA_PATH=/etc/ldap/schema
-UNI_TEMPLATE_PATH=/etc/univention/templates
-UNI_SCHEMA_PATH=/usr/share/univention-ldap/schema
-UNI_MODULE_PATH=/usr/lib/python2.4/site-packages/univention/admin/handers
+set -e
+set -u
 
 cd "`dirname $0`"
 
-echo "Installing schema..."
-install -m664 asterisk.schema "$LDAP_SCHEMA_PATH/"
+UNI_TEMPLATE_PATH=/etc/univention/templates
+UNI_SCHEMA_PATH=/usr/share/univention-ldap/schema
+UNI_MODULE_PATH=/usr/lib/python2.4/site-packages/univention/admin/handers
+UNI_ICON_PATH=/usr/share/univention-webui-style/icon
+
+echo -en "Installing schema...\t\ŧ"
+cd schema/
+install -m664 asterisk.schema "$UNI_SCHEMA_PATH/"
 install -m664 80asterisk "$UNI_TEMPLATE_PATH/files/etc/ldap/slapd.conf.d/"
-# info installieren
+install -m664 asterisk.info "$UNI_TEMPLATE_PATH/info/"
+cd ..
+echo "done."
 
+echo -en "Installing icons...\t\t"
+mkdir -p "$UNI_ICON_PATH/asterisk"
+install -m664 icons/asterisk/* "$UNI_ICON_PATH/asterisk/"
+echo "done."
 
-# icon
+echo -en "Installing UMC module...\t\t"
+mkdir -p "$UNI_MODULE_PATH/asterisk"
+install -m664 frontend/asterisk/* "$UNI_MODULE_PATH/asterisk/"
+echo "done."
 
 # ldap dir object default dings
 
-# asterisk/ nach modulepath
-
 # neustarten
-
 
