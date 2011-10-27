845aa0c8469b3347c7de5ef99595e1afaeb1c2a7
Tristan Bruns <bruns@decoit.de>
2011-10-27 09:29:07 +0200

    Fixt 1941: Wizard in UMC verlinken

--- settings.bash
+++ settings.bash
@@ -13,6 +13,7 @@ elif [ "$version_version" = "3.0" ]; then
 	UNI_SCHEMA_PATH=/usr/share/univention-ldap/schema
 	UNI_UDM_PATH=/usr/share/pyshared/univention/admin
 	UNI_UDM_LN_PATH=/usr/lib/pymodules/python2.6/univention/admin
+	UNI_UMC_PATH=/usr/share/univention-management-console
 	UNI_ICON_PATH=/var/www/univention-management-console/images/icons
 	UNI_REGINFO_PATH=/etc/univention/registry.info/
 	UNI_PYTHON=python2.6
