66f0662626f9c751998a24c7a6586f78aa457a43
Tristan Bruns <bruns@decoit.de>
2012-01-09 16:46:00 +0100

    bug 2183: Testdaten-Script erweitert

--- doc/testdaten-hinzufuegen.sh
+++ doc/testdaten-hinzufuegen.sh
@@ -23,12 +23,21 @@ create settings/service "Asterisk Server" "cn=services,cn=univention"
 create computers/memberserver "Fakemember" "cn=computers"
 
 # Testuser
-# todo
+create users/user "" "cn=users" \
+	--set lastname="Moss" \
+	--set firstname="Maurice" \
+	--set username="mmoss" \
+	--set password="mmoss" \
+	--set overridePWLength="1"
+create users/user "" "cn=users" \
+	--set lastname="Trenneman" \
+	--set firstname="Roy" \
+	--set username="rtrenneman" \
+	--set password="rtrenneman" \
+	--set overridePWLength="1"
 
-# Asterisk-Superordinates
+# =============== Telefonbuch =================================================
 create asterisk/phoneBook "Testtelefonbuch" "cn=asterisk"
-create asterisk/server "Testserver" "cn=asterisk" \
-	--set host="cn=Fakemember,cn=computers,$ldap_base"
 
 # Kontakte
 create asterisk/contact "Hans Dieter" "cn=Testtelefonbuch,cn=asterisk" \
@@ -36,10 +45,58 @@ create asterisk/contact "Hans Dieter" "cn=Testtelefonbuch,cn=asterisk" \
 create asterisk/contact "Peter Mueller" "cn=Testtelefonbuch,cn=asterisk" \
 	--set telephoneNumber="1234567890"
 
-# Asterisk-Krams
-# todo:
-# zwei telefone nebst mailboxen
-# telefontyp, konferenzraum
-# zwei telefongruppen
-# eine warteschlange
+# ========== Asterisk-Server ==================================================
+create asterisk/server "Testserver" "cn=asterisk" \
+	--set host="cn=Fakemember,cn=computers,$ldap_base"
+
+# Konferenzraum
+create asterisk/conferenceRoom "Konferenzraum" "cn=Testserver,cn=asterisk" \
+	--set extension="50" \
+	--set pin="1234" \
+	--set adminPin="4321"
+
+# Telefontyp
+create asterisk/phoneType "Grandstream 12D" "cn=Testserver,cn=asterisk" \
+	--set displaySize="ziemlich groß" \
+	--set manufacturer="Grandstream" \
+	--set type="Wählscheiben-Telefon"
+
+# Telefongruppen
+create asterisk/phoneGroup "Softwareentwicklung" "cn=Testserver,cn=asterisk" \
+	--set id="1"
+create asterisk/phoneGroup "Systemmanagement" "cn=Testserver,cn=asterisk" \
+	--set id="2"
+
+# Warteschleifen
+create asterisk/waitingLoop "Support-Hotline" "cn=Testserver,cn=asterisk" \
+	--set extension="50"
+
+# Mailboxen
+create asterisk/mailbox "" "cn=Testserver,cn=asterisk" \
+	--set id="20" \
+	--set password="1234" \
+	--set email="1"
+create asterisk/mailbox "" "cn=Testserver,cn=asterisk" \
+	--set id="21" \
+	--set password="1234" \
+	--set email="1"
+
+# Telefone
+create asterisk/sipPhone "" "cn=Testserver,cn=asterisk" \
+	--set extension="20" \
+	--set password="1234" \
+	--set phonetype="cn=Grandstream 12D,cn=Testserver,cn=asterisk,$ldap_base" \
+	--set waitingloops="cn=Support-Hotline,cn=Testserver,cn=asterisk" \
+	--set callgroups="cn=Systemmanagement,cn=Testserver,cn=asterisk" \
+	--set pickupgroups="cn=Systemmanagement,cn=Testserver,cn=asterisk" \
+	--set pickupgroups="cn=Softwareentwicklung,cn=Testserver,cn=asterisk"
+
+create asterisk/sipPhone "" "cn=Testserver,cn=asterisk" \
+	--set extension="21" \
+	--set password="1234" \
+	--set phonetype="cn=Grandstream 12D,cn=Testserver,cn=asterisk,$ldap_base"
+	--set waitingloops="cn=Support-Hotline,cn=Testserver,cn=asterisk" \
+	--set callgroups="cn=Softwareentwicklung,cn=Testserver,cn=asterisk" \
+	--set pickupgroups="cn=Softwareentwicklung,cn=Testserver,cn=asterisk" \
+	--set pickupgroups="cn=Systemmanagement,cn=Testserver,cn=asterisk"
 
