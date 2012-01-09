52fd90a89225d6cd4e8208810c910c49b2485376
Tristan Bruns <bruns@decoit.de>
2012-01-09 17:25:30 +0100

    bug 2183: Testdaten-Script erweitert

--- doc/testdaten-hinzufuegen.bash
+++ doc/testdaten-hinzufuegen.bash
@@ -7,69 +7,76 @@ eval "$(ucr shell)"
 
 function create {
 	module="$1"
-	name="$2"
-	position="$3"
-	shift 3
+	position="$2"
+	shift 2
 	udm $module create \
 		--ignore_exists \
 		--position "$position,$ldap_base" \
-		--set name="$name" \
-		--set commonName="$name" \
 		"$@"
 }
 
 # Memberserver-Fake
-create settings/service "Asterisk Server" "cn=services,cn=univention"
-create computers/memberserver "Fakemember" "cn=computers"
+create settings/service "cn=services,cn=univention" \
+	--set name="Asterisk Server"
+create computers/memberserver "cn=computers" \
+	--set name="Fakemember"
 
 # =============== Telefonbuch =================================================
-create asterisk/phoneBook "Testtelefonbuch" "cn=asterisk"
+create asterisk/phoneBook "cn=asterisk" \
+	--set commonName="Testtelefonbuch"
 
 # Kontakte
-create asterisk/contact "Hans Dieter" "cn=Testtelefonbuch,cn=asterisk" \
+create asterisk/contact "cn=Testtelefonbuch,cn=asterisk" \
+	--set commonName="Hans Dieter" \
 	--set telephoneNumber="0118 999 881 999 119 725 3"
-create asterisk/contact "Peter Mueller" "cn=Testtelefonbuch,cn=asterisk" \
+create asterisk/contact "cn=Testtelefonbuch,cn=asterisk" \
+	--set commonName="Peter Müller" \
 	--set telephoneNumber="1234567890"
 
 # ========== Asterisk-Server ==================================================
-create asterisk/server "Testserver" "cn=asterisk" \
+create asterisk/server "cn=asterisk" \
+	--set commonName="Testserver" \
 	--set host="cn=Fakemember,cn=computers,$ldap_base"
 
 # Konferenzraum
-create asterisk/conferenceRoom "Konferenzraum" "cn=Testserver,cn=asterisk" \
+create asterisk/conferenceRoom "cn=Testserver,cn=asterisk" \
+	--set commonName="Konferenzraum" \
 	--set extension="50" \
 	--set pin="1234" \
 	--set adminPin="4321"
 
 # Telefontyp
-create asterisk/phoneType "Grandstream 12D" "cn=Testserver,cn=asterisk" \
+create asterisk/phoneType "cn=Testserver,cn=asterisk" \
+	--set commonName="Grandstream 12D" \
 	--set displaySize="ziemlich groß" \
 	--set manufacturer="Grandstream" \
 	--set type="Wählscheiben-Telefon"
 
 # Telefongruppen
-create asterisk/phoneGroup "Softwareentwicklung" "cn=Testserver,cn=asterisk" \
+create asterisk/phoneGroup "cn=Testserver,cn=asterisk" \
+	--set commonName="Softwareentwicklung" \
 	--set id="1"
-create asterisk/phoneGroup "Systemmanagement" "cn=Testserver,cn=asterisk" \
+create asterisk/phoneGroup "cn=Testserver,cn=asterisk" \
+	--set commonName="Systemmanagement" \
 	--set id="2"
 
 # Warteschleifen
-create asterisk/waitingLoop "Support-Hotline" "cn=Testserver,cn=asterisk" \
-	--set extension="50" \
-	--set Name="Support-Hotline"
+create asterisk/waitingLoop "cn=Testserver,cn=asterisk" \
+	--set commonName="Support-Hotline" \
+	--set extension="50"
 
 # Mailboxen
-create asterisk/mailbox "" "cn=Testserver,cn=asterisk" \
+create asterisk/mailbox "cn=Testserver,cn=asterisk" \
 	--set id="20" \
 	--set password="1234" \
 	--set email="1"
-create asterisk/mailbox "" "cn=Testserver,cn=asterisk" \
+create asterisk/mailbox "cn=Testserver,cn=asterisk" \
 	--set id="21" \
 	--set password="1234" \
 	--set email="1"
 
 # Telefone
-create asterisk/sipPhone "" "cn=Testserver,cn=asterisk" \
+create asterisk/sipPhone "cn=Testserver,cn=asterisk" \
 	--set extension="20" \
 	--set password="1234" \
 	--set phonetype="cn=Grandstream 12D,cn=Testserver,cn=asterisk,$ldap_base" \
@@ -78,7 +85,7 @@ create asterisk/sipPhone "" "cn=Testserver,cn=asterisk" \
 	--set pickupgroups="cn=Systemmanagement,cn=Testserver,cn=asterisk" \
 	--set pickupgroups="cn=Softwareentwicklung,cn=Testserver,cn=asterisk"
 
-create asterisk/sipPhone "" "cn=Testserver,cn=asterisk" \
+create asterisk/sipPhone "cn=Testserver,cn=asterisk" \
 	--set extension="21" \
 	--set password="1234" \
 	--set phonetype="cn=Grandstream 12D,cn=Testserver,cn=asterisk,$ldap_base" \
@@ -89,7 +96,7 @@ create asterisk/sipPhone "" "cn=Testserver,cn=asterisk" \
 
 # ====================== Benutzer =============================================
 
-create users/user "" "cn=users" \
+create users/user "cn=users" \
 	--set lastname="Moss" \
 	--set firstname="Maurice" \
 	--set username="mmoss" \
@@ -99,7 +106,7 @@ udm users/user modify --dn "uid=mmoss,cn=users,$ldap_base" \
 	--set mailbox="cn=mailbox 20,cn=Testserver,cn=asterisk,$ldap_base" \
 	--set phones="cn=phone 20,cn=Testserver,cn=asterisk,$ldap_base"
 
-create users/user "" "cn=users" \
+create users/user "cn=users" \
 	--set lastname="Trenneman" \
 	--set firstname="Roy" \
 	--set username="rtrenneman" \
