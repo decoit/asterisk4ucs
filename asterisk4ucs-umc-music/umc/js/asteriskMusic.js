48f019cf4c8088fb5c4a066b2a5ae7fdc017cd93
Tristan Bruns <bruns@decoit.de>
2012-01-25 16:19:18 +0100

    umc-music: zeigt hello-world

--- asterisk4ucs-umc-music/umc/js/asteriskMusic.js
+++ asterisk4ucs-umc-music/umc/js/asteriskMusic.js
@@ -1,7 +1,24 @@
 
 dojo.provide("umc.modules.asteriskMusic");
 
-dojo.declare("umc.modules.asteriskMusic", [], {
+dojo.require("umc.widgets.Module");
+
+dojo.declare("umc.modules.asteriskMusic", [ umc.widgets.Module ], {
+	_page: null,
+	_form: null,
+
 	i18nClass: "umc.modules.asteriskMusic",
+
+	buildRendering: function () {
+		this.inherited(arguments);
+
+		this._page = new umc.widgets.Page({
+			headerText: "Hallo Welt!",
+			helpText: "Hallo, du digitalisierte Welt!",
+		});
+		this.addChild(this._page);
+
+		console.log(this);
+	},
 });
 
