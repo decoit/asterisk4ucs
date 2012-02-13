d5dc2844d44227b19576baa82395209cad1cc3fc
Tristan Bruns <bruns@decoit.de>
2012-02-13 17:06:08 +0100

    bug #1769: Alle GUI-Felder hinzugefügt

--- asterisk4ucs-umc-user/umc/js/asteriskUser.js
+++ asterisk4ucs-umc-user/umc/js/asteriskUser.js
@@ -5,6 +5,7 @@ dojo.require("umc.widgets.Module");
 dojo.require("umc.widgets.ContainerWidget");
 dojo.require("umc.widgets.Page");
 dojo.require("umc.widgets.Form");
+dojo.require("umc.widgets.Grid");
 
 dojo.declare("umc.modules.asteriskUser", [ umc.widgets.Module ], {
 	buildRendering: function () {
@@ -16,6 +17,8 @@ dojo.declare("umc.modules.asteriskUser", [ umc.widgets.Module ], {
 		this.addChild(tabContainer);
 
 		tabContainer.addChild(this.renderMailbox());
+		tabContainer.addChild(this.renderPhones());
+		tabContainer.addChild(this.renderForwarding());
 	},
 	renderMailbox: function () {
 		var page = new umc.widgets.Page({
@@ -61,6 +64,107 @@ dojo.declare("umc.modules.asteriskUser", [ umc.widgets.Module ], {
 			'mailbox/pin',
 			'mailbox/email',
 			'mailbox/timeout'
+		];
+
+		var form = new umc.widgets.Form({
+			widgets: widgets,
+			layout: layout,
+			scrollable: true,
+		});
+		page.addChild(form);
+
+		return page;
+	},
+	renderPhones: function () {
+		var page = new umc.widgets.Page({
+			title: "Telefone",
+			headerText: "Telefoneinstellungen",
+			closable: false,
+		});
+
+		var container = new umc.widgets.ContainerWidget({
+			scrollable: true,
+		});
+		page.addChild(container);
+
+		var widgets = [{
+			type: 'ComboBox',
+			name: 'phones/interval',
+			label: "Klingelintervall",
+			staticValues: [
+				{ id:   '2', label:  "2 Sekunden" },
+				{ id:   '4', label:  "4 Sekunden" },
+				{ id:   '6', label:  "6 Sekunden" },
+				{ id:   '8', label:  "8 Sekunden" },
+				{ id:  '10', label: "10 Sekunden" },
+			],
+		}, {
+			type: 'Grid',
+			name: 'phones/sequence',
+			label: "Klingelreihenfolge",
+			moduleStore: umc.store.getModuleStore("extension",
+				"asteriskUser/phones"),
+			query: {'*':'*'},
+			columns: [{
+				name: 'name',
+				label: "Telefon",
+				editable: false,
+			}],
+			actions: [{
+				name: 'earlier',
+				label: "Früher",
+				canExecute: dojo.hitch(this, function (values) {
+					return true;
+				}),
+				callback: dojo.hitch(this, function (id) {
+					alert("frueher" + id);
+				}),
+			}, {
+				name: 'later',
+				label: "Später",
+				canExecute: dojo.hitch(this, function (values) {
+					return true;
+				}),
+				callback: dojo.hitch(this, function (id) {
+					alert("spaeter" + id);
+				}),
+			}],
+		}];
+
+		var layout = [
+			'phones/interval',
+			'phones/sequence',
+		];
+
+		var form = new umc.widgets.Form({
+			widgets: widgets,
+			layout: layout,
+			scrollable: true,
+		});
+		page.addChild(form);
+
+		return page;
+	},
+	renderForwarding: function () {
+		var page = new umc.widgets.Page({
+			title: "Weiterleitung",
+			headerText: "Weiterleitungseinstellungen",
+			closable: false,
+		});
+
+		var container = new umc.widgets.ContainerWidget({
+			scrollable: true,
+		});
+		page.addChild(container);
+
+		var widgets = [{
+			type: 'TextBox',
+			name: 'forwarding/number',
+			label: "Weiterleitungsziel",
+		}];
+
+		var layout = [
+			'forwarding/number'
 		];
 
 		var form = new umc.widgets.Form({
--- asterisk4ucs-umc-user/umc/python/asteriskUser/__init__.py
+++ asterisk4ucs-umc-user/umc/python/asteriskUser/__init__.py
@@ -1,4 +1,5 @@
 import univention.management.console.modules
+from univention.management.console.protocol.definitions import SUCCESS
 
 class Instance( univention.management.console.modules.Base ):
     def getSettings( self, request ):
@@ -6,4 +7,11 @@ class Instance( univention.management.console.modules.Base ):
 
     def setSettings( self, request ):
         self.finished( request.id, "success" )
+
+    def phonesQuery(self, request):
+        request.status = SUCCESS
+        self.finished(request.id, [{
+            extension: 42,
+            name: "Telefon 42",
+        }])
 
