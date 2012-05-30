62d7cfc89fec2c88eb82c09a0505c269e0e825d7
Tristan Bruns <bruns@decoit.de>
2012-05-30 16:41:06 +0200

    bug 2747: umc-modul für deployment

---
+++ asterisk4ucs-umc-deploy/umc/js/asteriskDeploy.js
@@ -0,0 +1,148 @@
+/*
+Copyright (C) 2012 DECOIT GmbH <asterisk4ucs@decoit.de>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as
+published by the Free Software Foundation
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+dojo.provide("umc.modules.asteriskDeploy");
+
+dojo.require("umc.widgets.Module");
+
+dojo.declare("umc.modules.asteriskDeploy", [ umc.widgets.Module ], {
+	_page: null,
+	_form: null,
+	_serverSelect: null,
+	_serverdn: null,
+
+	i18nClass: "umc.modules.asteriskDeploy",
+
+	buildRendering: function () {
+		this.inherited(arguments);
+
+		this._page = new umc.widgets.Page({
+			headerText: "Deployment von Asterisk-Konfigurationen",
+		});
+		this.addChild(this._page);
+
+		var widgets = [{
+			type: 'ComboBox',
+			name: 'server',
+			label: "Server",
+			dynamicValues: "asteriskDeploy/queryServers",
+		}, {
+			type: 'Button',
+			name: 'copyid',
+			label: "SSH-Schlüssel kopieren",
+			callback: dojo.hitch(this, function () {
+				var rootpw = prompt("Root-Passwort des Asterisk-Servers?");
+				if (!rootpw || !rootpw.length)
+					return;
+
+				this._startAction("asteriskDeploy/copyid", {
+					server: this._serverdn,
+					rootpw: rootpw,
+				});
+			}),
+		}, {
+			type: 'Button',
+			name: 'create',
+			label: "Konfiguration testen",
+			callback: dojo.hitch(this, function () {
+				this._startAction("asteriskDeploy/create", {
+					server: this._serverdn,
+				});
+			}),
+		}, {
+			type: 'Button',
+			name: 'deploy',
+			label: "Konfiguration anwenden",
+			callback: dojo.hitch(this, function () {
+				this._startAction("asteriskDeploy/deploy", {
+					server: this._serverdn,
+				});
+			}),
+		}];
+
+		var layout = [
+			[ "server", "copyid" ],
+			[ "create", "deploy" ],
+		];
+
+		this._form = new umc.widgets.Form({
+			widgets: widgets,
+			layout: layout,
+			region: "top",
+		});
+		this._page.addChild(this._form);
+		
+		this._log = new umc.widgets.Text({
+			style: "font-family: monospace; overflow: scroll; white-space: pre;",
+		});
+
+		var container = new umc.widgets.ExpandingTitlePane({
+			title: "Letzte Logdatei",
+		});
+		this._page.addChild(container);
+		container.addChild(this._log);
+
+		asteriskDeploy = this;
+	},
+	postCreate: function () {
+		this.inherited(arguments);
+
+		this._serverSelect = this._form.getWidget("server");
+		this._serverdn = this._serverSelect.getValue();
+
+		dojo.connect(this._serverSelect, "onChange", dojo.hitch(this, function () {
+			this._setServer(this._serverSelect.getValue());
+		}));
+	},
+	_setServer: function (serverdn) {
+		this._serverdn = serverdn;
+		this._refreshLog();
+	},
+	_startAction: function (action, args) {
+		this._form.getWidget("server")._setDisabledAttr(true);
+		this._form.getWidget("copyid")._setDisabledAttr(true);
+		this._form.getWidget("create")._setDisabledAttr(true);
+		this._form.getWidget("deploy")._setDisabledAttr(true);
+		
+		var call = this.umcpCommand(action, args);
+		call.then(dojo.hitch(this, function (data) {
+			this._stopAction();
+			umc.dialog.notify("Befehl ausgeführt; Siehe Logdatei im unteren Teil des Fensters");
+		}), dojo.hitch(this, function (data) {
+			this._stopAction();
+			umc.dialog.notify("Fehler!");
+		}));
+	},
+	_stopAction: function () {
+		this._form.getWidget("server")._setDisabledAttr(false);
+		this._form.getWidget("copyid")._setDisabledAttr(false);
+		this._form.getWidget("create")._setDisabledAttr(false);
+		this._form.getWidget("deploy")._setDisabledAttr(false);
+
+		this._refreshLog();
+	},
+	_refreshLog: function () {
+		this._log._setContentAttr("[Logdatei lädt...]");
+
+		var call = this.umcpCommand("asteriskDeploy/getLog", {
+			server: this._serverdn,
+		});
+		call.then(dojo.hitch(this, function (data) {
+			this._log._setContentAttr(data.result);
+		}));
+	},
+});
---
+++ asterisk4ucs-umc-deploy/umc/python/asteriskDeploy/__init__.py
@@ -0,0 +1,204 @@
+"""
+Copyright (C) 2012 DECOIT GmbH <asterisk4ucs@decoit.de>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as
+published by the Free Software Foundation
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+import univention.management.console.modules
+from univention.management.console.protocol.definitions import SUCCESS
+
+import univention.config_registry
+import univention.admin.uldap
+import univention.admin.config
+
+import univention.admin.modules
+univention.admin.modules.update()
+
+import univention.admin.handlers.asterisk
+import univention.admin.handlers.asterisk.server
+import univention.admin.handlers.asterisk.music
+
+import re
+from time import strftime, localtime, sleep
+import shutil
+import tempfile
+import subprocess
+
+class Instance(univention.management.console.modules.Base):
+	def queryServers(self, request):
+		servers = getServers()
+
+		result = []
+		for server in servers:
+			result.append({
+				"id": server.dn,
+				"label": server["commonName"],
+			})
+
+		self.finished(request.id, result)
+
+	def copyid(self, request):
+		server = getServer(request.options["server"])
+		log = openLog(server["commonName"])
+		print >>log, "Die Funktion zum Kopieren des SSH-Schlüssels ist noch " \
+				"nicht implementiert."
+		closeLog(log)
+		self.finished(request.id, True)
+
+	def create(self, request):
+		server = getServer(request.options["server"])
+		log = openLog(server["commonName"])
+		createConfigs(log, server)
+		closeLog(log)
+		self.finished(request.id, True)
+
+	def deploy(self, request):
+		server = getServer(request.options["server"])
+		log = openLog(server["commonName"])
+
+		configs = createConfigs(log, server)
+		print >>log, "\n\n"
+		try:
+			deployConfigs(log, server, configs)
+		except subprocess.CalledProcessError:
+			print >>log, "\n\nERROR: Aborted deployment because of an error.\n"
+		else:
+			print >>log, "\n\nDie neue Konfiguration ist jetzt aktiv."
+			print >>log, "Viel Spaß beim Telefonieren!\n"
+
+		closeLog(log)
+		self.finished(request.id, True)
+
+	def getLog(self, request):
+		server = getServer(request.options["server"])
+		log = getLog(server["commonName"])
+		self.finished(request.id, log)
+
+
+def getCoLoPos():
+	co = univention.admin.config.config()
+
+	lo, pos = univention.admin.uldap.getAdminConnection()
+
+	return co, lo, pos
+
+def getServers():
+	co, lo, pos = getCoLoPos()
+
+	server = univention.admin.modules.get("asterisk/server")
+	univention.admin.modules.init(lo, pos, server)
+	servers = server.lookup(co, lo, None)
+
+	for item in servers:
+		item.open()
+
+	return servers
+
+def getServer(dn):
+	co, lo, pos = getCoLoPos()
+
+	server = univention.admin.modules.get("asterisk/server")
+	univention.admin.modules.init(lo, pos, server)
+	obj = server.object(co, lo, None, dn)
+	obj.open()
+
+	return obj
+
+def getLog(servername):
+	servername = re.sub(r"[^a-zA-Z0-9]+", "_", servername)
+	filename = "/var/log/univention/asteriskDeploy-%s.log" % servername
+	try:
+		return open(filename).read()
+	except IOError, e:
+		return "[Error: %s]" % str(e)
+
+def openLog(servername):
+	servername = re.sub(r"[^a-zA-Z0-9]+", "_", servername)
+	filename = "/var/log/univention/asteriskDeploy-%s.log" % servername
+	time = strftime("%a, %d %b %Y %H:%M:%S", localtime())
+	
+	log = open(filename, "w", False)
+	log.write("Log opened %s\n" % time)
+	log.write("===========%s\n" % ("=" * len(time)))
+	log.write("\n")
+	return log
+
+def closeLog(log):
+	time = strftime("%a, %d %b %Y %H:%M:%S", localtime())
+	log.write("\n")
+	log.write("Log closed %s\n" % time)
+	log.close()
+
+def createConfigs(log, server):
+	print >>log, "Creating config files for server %s" % server["commonName"]
+	print >>log, "=================================%s" % (
+			"=" * len(server["commonName"]))
+	
+	configs = univention.admin.handlers.asterisk.genConfigs(server)
+	for name, data in configs.items():
+		print >>log
+		print >>log
+		print >>log, "Creating %s:" % name
+		print >>log, "---------%s-" % ("-" * len(name))
+		print >>log
+		errors = re.findall(r"^;; .*", data, re.MULTILINE)
+		for error in errors:
+			print >>log, error[3:]
+		if not errors:
+			print >>log, "(no error)"
+
+	return configs
+
+def logCall(log, args, cwd):
+	print >>log, "Creating child process: %s" % str(args)
+	proc = subprocess.Popen(args, stdout=log, stderr=log,
+			stdin=open("/dev/zero"), cwd=cwd)
+	print >>log, "Child process has pid %i" % proc.pid
+
+	sleeptime = 0.1
+	timeleft = 30
+	while timeleft > 0:
+		if proc.poll() != None:
+			break
+		sleep(sleeptime)
+		timeleft -= sleeptime
+	else:
+		print >>log, "Child process did not terminate in time, killing it..."
+		proc.kill()
+		raise subprocess.CalledProcessError(proc.returncode, args[0])
+
+	print >>log, "Child returned code %i" % proc.returncode
+
+	if proc.returncode != 0:
+		raise subprocess.CalledProcessError(proc.returncode, args[0])
+
+def deployConfigs(log, server, configs):
+	print >>log, "Deploying config files to server %s" % server["commonName"]
+	print >>log, "=================================%s" % (
+			"=" * len(server["commonName"]))
+	print >>log
+	
+	sshtarget = "%s@%s" % (server["sshuser"], server["sshhost"])
+	scptarget = "%s:%s/ucs_autogen" % (sshtarget, server["sshpath"])
+	sshcmd = server["sshcmd"]
+
+	tmpdir = tempfile.mkdtemp()
+	try:
+		for name, data in configs.items():
+			open("%s/%s" % (tmpdir, name), "w").write(data)
+
+		logCall(log, ["scp", "-Bq"] + configs.keys() + [scptarget], tmpdir)
+		logCall(log, ["ssh", "-oBatchMode=yes", sshtarget, sshcmd], tmpdir)
+	finally:
+		shutil.rmtree(tmpdir)
